fcf7ef0a52291469a8ae33c37f76aa1b6a5f17b5
nadamovich <nikita.adamovich@gmail.com>
2022-02-06 19:07:16 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let i = 0;
+    let nToAbsoluteValue = Math.abs (n);
+    let nToSting = nToAbsoluteValue.toString();    
+    let reversedNumber = '';    
+    while (i < nToSting.length) {
+        reversedNumber = nToSting[i] + reversedNumber;
+        i = i + 1;
+    }
+    return reversedNumber;
 }
